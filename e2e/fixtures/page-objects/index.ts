31dc18de3ea71ca76962dc6887403cb805e7f62f
Brandon Geraci <brandon.geraci@gmail.com>
2026-02-21 09:15:19 -0600

    feat(e2e): add core page object models (Login, Dashboard, Repos, Packages, Users, Groups)

---
+++ e2e/fixtures/page-objects/index.ts
@@ -0,0 +1,6 @@
+export { LoginPage } from './LoginPage';
+export { DashboardPage } from './DashboardPage';
+export { RepositoriesPage } from './RepositoriesPage';
+export { PackagesPage } from './PackagesPage';
+export { UsersPage } from './UsersPage';
+export { GroupsPage } from './GroupsPage';
