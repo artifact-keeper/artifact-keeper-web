51be8ec9bafdf699fced8d6383964b0436084f7d
Brandon Geraci <brandon.geraci@gmail.com>
2026-02-24 15:06:30 -0600

    chore(deps): upgrade react-resizable-panels to v4
    Migrates to v4 API:
    - PanelGroup -> Group, PanelResizeHandle -> Separator
    - direction prop -> orientation prop
    - Removes autoSaveId (deprecated in v4)
    
    Closes #85

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -2,12 +2,12 @@
 
 import { GripVerticalIcon } from "lucide-react"
 import {
+  Group,
   Panel,
-  PanelGroup,
-  PanelResizeHandle,
-  type PanelGroupProps,
+  Separator,
+  type GroupProps,
   type PanelProps,
-  type PanelResizeHandleProps,
+  type SeparatorProps,
 } from "react-resizable-panels"
 
 import { cn } from "@/lib/utils"
@@ -15,9 +15,9 @@ import { cn } from "@/lib/utils"
 function ResizablePanelGroup({
   className,
   ...props
-}: PanelGroupProps) {
+}: GroupProps) {
   return (
-    <PanelGroup
+    <Group
       data-slot="resizable-panel-group"
       className={cn(
         "flex h-full w-full aria-[orientation=vertical]:flex-col",
@@ -36,11 +36,11 @@ function ResizableHandle({
   withHandle,
   className,
   ...props
-}: PanelResizeHandleProps & {
+}: SeparatorProps & {
   withHandle?: boolean
 }) {
   return (
-    <PanelResizeHandle
+    <Separator
       data-slot="resizable-handle"
       className={cn(
         "bg-border focus-visible:ring-ring relative flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden aria-[orientation=horizontal]:h-px aria-[orientation=horizontal]:w-full aria-[orientation=horizontal]:after:left-0 aria-[orientation=horizontal]:after:h-1 aria-[orientation=horizontal]:after:w-full aria-[orientation=horizontal]:after:translate-x-0 aria-[orientation=horizontal]:after:-translate-y-1/2 [&[aria-orientation=horizontal]>div]:rotate-90",
@@ -53,7 +53,7 @@ function ResizableHandle({
           <GripVerticalIcon className="size-2.5" />
         </div>
       )}
-    </PanelResizeHandle>
+    </Separator>
   )
 }
 
